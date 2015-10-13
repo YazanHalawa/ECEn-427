71d2ccf340b136c915b1809ef131a56f54fccbe8
Yazan Halawa <y.halawa94@gmail.com>
2015-10-12 23:36:55 -0600

    Lab 4

---
+++ Lab4/Lab4_Workspace/hello_world_0/Debug/src/render.d
@@ -0,0 +1,8 @@
+src/render.d src/render.o: ../src/render.c ../src/render.h \
+  ../src/globals.h ../src/bitmaps.h
+
+../src/render.h:
+
+../src/globals.h:
+
+../src/bitmaps.h:
---
+++ Lab4/Lab4_Workspace/hello_world_0/src/interrupts.h
@@ -0,0 +1,13 @@
+/*
+ * interrupts.h
+ *
+ *  Created on: Oct 12, 2015
+ *      Author: superman
+ */
+
+#ifndef INTERRUPTS_H_
+#define INTERRUPTS_H_
+
+int interruptManager (unsigned int * framePointer0);
+
+#endif /* INTERRUPTS_H_ */
